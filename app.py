d89f0bf15cab03f53ec032a8861a1910b030e725
Jeremy Tomasi <jeremy.tomasi.pro@gmail.com>
2024-01-06 23:37:44 +0100

    Register function added

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_wtf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
+    db = SQLAlchemy()
     csrf = CSRFProtect()
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "projetM1"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://projet:projet@db:5432/projet_etrs711"
     csrf.init_app(app)
     Bootstrap(app)
-    return app
+    db.init_app(app)
+    return app, db
 
-app = create_app()
+app, db = create_app()
 
 # Routes
 import src.controllers.HomeController
--- src/controllers/RegisterController.py
+++ src/controllers/RegisterController.py
@@ -1,8 +1,35 @@
-from app import app
+from sqlalchemy.exc import IntegrityError
+
+from app import app, db
 from flask import render_template, url_for
+
+import hashlib
+
+from src.models.database.User import User
 from src.models.forms.RegisterForm import RegisterForm
 
-@app.route("/register")
+@app.route("/register",methods=["GET"])
 def show_register_page():
     form = RegisterForm()
     return render_template('register.html',form=form)
+
+@app.route("/register",methods=["POST"])
+def register_user():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = hashlib.sha1(str(form.password.data).encode()).hexdigest()
+
+        user = User(username,password)
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+            success = True
+            error = False
+        except IntegrityError:
+            error = True
+            success = False
+            db.session.rollback()
+
+    return render_template("register.html",form=form,success=success,error=error)
