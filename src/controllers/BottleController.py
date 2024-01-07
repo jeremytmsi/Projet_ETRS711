5d9e59447de9a2fec86e6369a0b680388a173b42
Jeremy Tomasi <jeremy.tomasi.pro@gmail.com>
2024-01-07 13:02:04 +0100

    Delete bottle from it's page function added

--- src/controllers/BottleController.py
+++ src/controllers/BottleController.py
@@ -4,6 +4,8 @@ from flask import render_template, url_for, flash, redirect
 from flask_login import login_required, current_user
 
 from src.models.database.Bottle import Bottle
+from src.models.forms.BottleDeleteForm import BottleDeleteForm
+
 
 def get_bottle_by_id(id):
     return db.session.execute(db.select(Bottle).filter_by(id=id)).scalars().first()
@@ -11,4 +13,12 @@ def get_bottle_by_id(id):
 @app.route("/profile/bottle/<id>")
 def show_detailed_bottle(id):
     bottle = get_bottle_by_id(id)
-    return render_template("bottle.html", bottle=bottle)
+    deleteForm = BottleDeleteForm()
+    return render_template("bottle.html",deleteForm = deleteForm, bottle=bottle)
+
+@app.route("/profile/bottle/<id>/delete", methods=["POST"])
+def delete_bottle(id):
+    bottle = get_bottle_by_id(id)
+    db.session.delete(bottle)
+    db.session.commit()
+    return redirect(url_for("get_detailed_shelf", id=bottle.shelf_id))
